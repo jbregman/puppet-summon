832f5c34c69cafa42409fa63824053d7ee2429ec
Josh Bregman <jbregman@gmail.com>
2016-03-20 00:47:41 -0400

    Added CLI installation

--- jbregman-summon/manifests/provider/conjur.pp
+++ jbregman-summon/manifests/provider/conjur.pp
@@ -16,5 +16,9 @@ define summon::provider::conjur {
 			require => [Exec['download-summon-conjur']],
 		}
 
+		package {'conjur-cli':
+			source => 'https://github.com/conjurinc/cli-ruby/releases/download/v4.30.1/conjur-4.30.1-1.pkg'
+		}
+
 	}
 }
